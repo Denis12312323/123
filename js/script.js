f868e712cc3bf1230230b5bcfccf99c915d65e03
Denis12312323 <uc4iha.sora@gmail.com>
2023-05-28 13:11:26 +0300

    site

---
+++ js/script.js
@@ -0,0 +1,167 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener("resize", (e) => {
+    e.preventDefault();
+
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+var swiper = new Swiper(".swiper", {
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 8
+        },
+        576: {
+            slidesPerView: 4,
+            spaceBetween: 16
+        }
+    },
+    spaceBetween: 16,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        bulletClass: "swiper-pagination-bullet",
+        bulletActiveClass: "swiper-pagination-bullet-active",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-nextt",
+        prevEl: ".swiper-button-previous",
+    },
+});
+
+
+let circles = document.querySelector(".circles");
+if (circles) {
+    let colors = ["#BFD7EA", "#4ECDC4", "#4062BB", "#E0FF4F"];
+    for (let i = 0; i < colors.length; i++) {
+        let circleDiv = document.createElement("div");
+        circleDiv.className = "color-circle";
+        circleDiv.style.backgroundColor = colors[i];
+        circleDiv.style.top = (10 + 90 * (i / colors.length)) + "%";
+        if (i % 2 == 0) {
+            circleDiv.style.right = "64px";
+        } else {
+            circleDiv.style.left = "64px";
+        }
+
+        document.body.prepend(circleDiv);
+    }
+}
+
+let regionPaths = document.querySelectorAll(".ukraine-map-wrapper svg path");
+if (regionPaths) {
+    let modalOverlay = document.createElement("div");
+    modalOverlay.className = "modal-overlay";
+    modalOverlay.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        document.body.style.overflow = "auto";
+        modalOverlay.classList.remove("active");
+        modal.classList.remove("active");
+    });
+
+    let modal = document.createElement("div");
+    modal.className = "modal";
+
+    let modalImage = document.createElement("img");
+    modalImage.className = "modal-image";
+    let modalTitle = document.createElement("a");
+    modalTitle.className = "modal-title";
+    let modalText = document.createElement("p");
+    modalText.className = "modal-text";
+    let modalGoToButton = document.createElement("a");
+    modalGoToButton.className = "modal-go-to-button";
+    let goToButtonArrow = '<svg width="17" height="12" viewBox="0 0 17 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.4687 10.7812C10.3437 10.6562 10.25 10.4687 10.25 10.25C10.25 10.0625 10.3437 9.875 10.5 9.75L13.4687 6.75L0.75 6.75C0.34375 6.75 -1.99433e-07 6.4375 -2.18557e-07 6C-2.36315e-07 5.59375 0.34375 5.25 0.75 5.25L13.4687 5.25L10.5 2.28125C10.1875 2 10.1875 1.53125 10.5 1.25C10.7812 0.9375 11.25 0.9375 11.5625 1.25L15.8125 5.5C16.0937 5.78125 16.0937 6.25 15.8125 6.53125L11.5625 10.7812C11.25 11.0937 10.7813 11.0937 10.4687 10.7812Z" fill="#1A191C" /></svg>';
+
+    modal.append(modalImage, modalTitle, modalText, modalGoToButton);
+
+    document.body.append(modalOverlay, modal);
+
+    let regionHint = document.createElement("div");
+    regionHint.className = "region-hint";
+
+    regionPaths.forEach(regionPath => {
+        regionPath.onmouseover = () => {
+            regionHint.innerHTML = regionPath.getAttribute("data-region-title");
+            document.body.append(regionHint);
+
+            regionPath.addEventListener("mousemove", (e) => {
+                e.preventDefault();
+
+                regionHint.style.top = e.pageY + 1 + "px";
+                if (window.innerWidth > 767) {
+                    regionHint.style.left = e.pageX - 1 + "px";
+                } else {
+                    regionHint.style.left = window.innerWidth / 2 + "px";
+                }
+            }, false);
+        }
+
+        regionPath.onmouseout = () => {
+            document.body.removeChild(regionHint);
+        }
+
+        regionPath.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            modalImage.src = regionPath.getAttribute("data-modal-image");
+            modalImage.alt = regionPath.getAttribute("data-region-title");
+
+            modalTitle.innerHTML = regionPath.getAttribute("data-region-title");
+            modalTitle.href = "regions/" + regionPath.getAttribute("data-region-title").toLowerCase() + ".html";
+
+            modalText.innerHTML = regionPath.getAttribute("data-modal-text");
+
+            modalGoToButton.innerHTML = "Go to " + regionPath.getAttribute("data-region-title") + goToButtonArrow;
+            modalGoToButton.href = "regions/" + regionPath.getAttribute("data-region-title").toLowerCase() + ".html";
+
+            document.body.style.overflow = "hidden";
+            modalOverlay.classList.add("active");
+            modal.classList.add("active");
+
+            document.body.addEventListener("keyup", (e) => {
+                if (e.key == "Escape") {
+                    document.body.style.overflow = "auto";
+                    modalOverlay.classList.remove("active");
+                    modal.classList.remove("active");
+                }
+            }, false);
+        });
+    });
+}
+// Подсвечивание текущей  Странички
+let navLinks = document.querySelectorAll(".header_link");
+navLinks.forEach(link => {
+    let linkPathname = link.pathname;
+    let pathname = window.location.pathname;
+    if (linkPathname == pathname) {
+        link.classList.add("current");
+    } else if (pathname == "/cooking/" || pathname == "/") {
+        navLinks[0].classList.add("current");
+    } else if (pathname == "/regions/kharkiv.html" || pathname == "/regions/kyiv.html" || pathname == "/regions/cherkasy.html" || pathname == "/regions/chernihiv.html" || pathname == "/regions/chernivtsi.html" || pathname == "/regions/crimea.html" || pathname == "/regions/dnipro.html" || pathname == "/regions/donetsk.html" || pathname == "/regions/ivano-frankivsk.html" || pathname == "/regions/kherson.html" || pathname == "/regions/khmelnytskyi.html" || pathname == "/regions/kropyvnytskyi.html" || pathname == "/regions/lutsk.html" || pathname == "/regions/lviv.html" || pathname == "/regions/mykolaiv.html" || pathname == "/regions/odesa.html" || pathname == "/regions/poltava.html" || pathname == "/regions/rivne.html" || pathname == "/regions/sumy.html" || pathname == "/regions/ternopil.html" || pathname == "/regions/uzhhorod.html" || pathname == "/regions/vinnitsa.html" || pathname == "/regions/zaporizhzhia.html" || pathname == "/regions/zhytomyr.html") {
+        navLinks[1].classList.add("current");
+    }
+});
+
+let watchVideoBtn = document.querySelector('.media__button');
+let mediaImg = document.querySelector('.media-img');
+let mediaVideo = document.querySelector('.media-video');
+
+if (watchVideoBtn) {
+    watchVideoBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        mediaImg.classList.add('hidden');
+        setTimeout(() => {
+            mediaImg.style.display = "none";
+            mediaVideo.style.display = "block";
+            setTimeout(() => {
+                mediaVideo.classList.remove("hidden");
+            }, 50);
+        }, 300);
+    })
+}
